890457a9ec4489856a643e80ca14f6a6bc14f1db
TJ Smaldone <tjsmaldone@gmail.com>
2016-06-23 17:02:40 -0400

    Commit 1
    Completed Project

---
+++ Assets/_Scripts/Shield.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shield : MonoBehaviour {
+
+	public float rotationsPerSecond = 0.1f;
+	public bool __________________;
+	public int levelShown = 0;
+
+	void Start () {
+		
+	}
+	
+	void Update () 
+	{
+		int currLevel = Mathf.FloorToInt (Hero.S.shieldLevel);
+		if (levelShown != currLevel)
+		{
+			levelShown = currLevel;
+			Material mat = this.renderer.material;
+			mat.mainTextureOffset = new Vector2(0.2f*levelShown, 0);
+		}
+		float rZ = (rotationsPerSecond * Time.time * 360) % 360f;
+		transform.rotation = Quaternion.Euler (0, 0, rZ);  
+	}
+}
